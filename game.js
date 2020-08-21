5a03ac91a8c0cac9285761af554b714ff76af200
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2020-08-21 16:35:46 -0500

    animation feedback added

--- game.js
+++ game.js
@@ -76,8 +76,18 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
-        getNewQuestion();
+
+        let classToApply = 'incorrect';
+            if (selectedAnswer == currentQuestion.answer) {
+                classToApply = 'correct';
+            }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+        
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();            
+        }, 1000);
     });
 });
 
