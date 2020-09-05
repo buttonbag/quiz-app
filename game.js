02add8ab31e9d483e151011d62475167f03e4fdc
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2020-09-05 07:35:47 -0500

    game now pulling from opendb.com for trivia

--- game.js
+++ game.js
@@ -12,21 +12,30 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch("questions.json")
-    .then(res => {
-        return res.json();
-    })
-    .then(loadedQuestions => {
-        console.log(loadedQuestions);
-        questions = loadedQuestions;
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.results);
+        questions = data.results.map( loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random()*3)+1;
+            answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index+1)] = choice;
+            })
+            return formattedQuestion;
+        })
         startGame();
-    }).catch(err => {
-        console.error(err);
-});
+    });
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
     questionCounter = 0;
